72d5e162e45aeee270c7fd7e5367bffd6f1fac7e
Daniyar234 <belgibekovdaniar@gmail.com>
2026-02-13 18:54:28 +0500

    User stories - second part, push #2 FINAL

--- src/lwc/itemPurchaseTool/itemPurchaseTool.js
+++ src/lwc/itemPurchaseTool/itemPurchaseTool.js
@@ -2,38 +2,36 @@
  * Created by Dream on 13.02.2026.
  */
 
-import {LightningElement, api, wire} from 'lwc';
-import {getRecord, getFieldValue} from 'lightning/uiRecordApi';
+import { LightningElement, wire } from 'lwc';
+import { CurrentPageReference } from 'lightning/navigation';
+import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
 
 import NAME_FIELD from '@salesforce/schema/Account.Name';
 import NUMBER_FIELD from '@salesforce/schema/Account.AccountNumber';
 import INDUSTRY_FIELD from '@salesforce/schema/Account.Industry';
 
-
 export default class ItemPurchaseTool extends LightningElement {
-    @api recordId;
+    accountId;
 
-    @wire(getRecord, {
-        recordId: '$recordId',
-        fields: [NAME_FIELD, NUMBER_FIELD, INDUSTRY_FIELD]
-    })
+    @wire(CurrentPageReference)
+    getStateParameters(currentPageReference) {
+        if (currentPageReference) {
+            this.accountId = currentPageReference.state.c__accountId;
+        }
+    }
+
+    @wire(getRecord, { recordId: '$accountId', fields: [NAME_FIELD, NUMBER_FIELD, INDUSTRY_FIELD] })
     account;
 
     get accName() {
-        return this.account.data
-            ? getFieldValue(this.account.data, NAME_FIELD)
-            : '';
+        return getFieldValue(this.account.data, NAME_FIELD);
     }
 
     get accNumber() {
-        return this.account.data
-        ? getFieldValue(this.account.data, NUMBER_FIELD)
-        : '';
+        return getFieldValue(this.account.data, NUMBER_FIELD);
     }
 
     get accIndustry() {
-        return this.account.data
-            ? getFieldValue(this.account.data, INDUSTRY_FIELD)
-            : '';
+        return getFieldValue(this.account.data, INDUSTRY_FIELD);
     }
 }
